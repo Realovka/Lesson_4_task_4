db6ca62bc469786f87f7269148689830e6640cc4
Nastya <realovka@mail.ru>
2020-05-13 00:26:53 +0300

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,25 @@
+package com.company;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] array = new int[20];
+        Random random = new Random();
+        for (int i = 0; i < array.length; i++) {
+            array[i] = random.nextInt(20);
+        }
+
+        System.out.print(Arrays.toString(array));
+
+        for (int i = 0; i < array.length; i++) {
+            if (i % 2 != 0) {
+                array[i] = 0;
+            }
+        }
+        System.out.println();
+        System.out.print(Arrays.toString(array));
+    }
+}
